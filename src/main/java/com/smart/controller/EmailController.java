e15efe3f20ac460d02d203bab6021e96e782a173
Altaf_Hossain <44937513+Altafhossain72@users.noreply.github.com>
2021-01-05 14:27:16 +0600

    added email
    added email services

---
+++ src/main/java/com/smart/controller/EmailController.java
@@ -0,0 +1,35 @@
+package com.smart.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.smart.model.EmailRequest;
+import com.smart.services.EmailService;
+
+
+@RestController
+public class EmailController {
+
+	@Autowired
+	private EmailService emailService;
+	
+	@PostMapping("/sendSuccesEmail")
+	public ResponseEntity<?> sendSuccesEmail(@ModelAttribute("emailRequest") EmailRequest emailRequest){
+		boolean result = this.emailService.sendEmail(emailRequest.getSubject(), emailRequest.getMessage(), emailRequest.getTo());
+		
+		if(result) {
+			return ResponseEntity.ok("Email sent successfully...");
+		}
+		
+		else {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body( "Email sent failed");
+		}
+		
+		
+	}
+}
