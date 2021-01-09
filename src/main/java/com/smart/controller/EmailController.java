7fe8881c4f09f045693c4d8ebf5a2e102857f614
Altaf_Hossain <44937513+Altafhossain72@users.noreply.github.com>
2021-01-09 11:31:56 +0600

    adding email varification
    added email varifications

--- src/main/java/com/smart/controller/EmailController.java
+++ src/main/java/com/smart/controller/EmailController.java
@@ -1,19 +1,23 @@
 package com.smart.controller;
 
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.smart.dao.UserRepository;
+import com.smart.helper.Message;
 import com.smart.model.EmailRequest;
 import com.smart.services.EmailService;
 
 
-@RestController
+@Controller
 public class EmailController {
 
 	@Autowired
@@ -21,17 +25,18 @@ public class EmailController {
 	
 
 	@PostMapping("/sendSuccesEmail")
-	public ResponseEntity<?> sendSuccesEmail(@ModelAttribute("emailRequest") EmailRequest emailRequest){
+	public String sendSuccesEmail(@ModelAttribute("emailRequest") EmailRequest emailRequest, HttpSession session){
 		boolean result = this.emailService.sendEmailWithAttach(emailRequest.getSubject(), emailRequest.getMessage(),emailRequest.getAttFile() ,emailRequest.getTo());
 		
 		if(result) {
-			return ResponseEntity.ok("Email sent successfully...");
+			session.setAttribute("message", new Message("Email sent Successfully  !! ", "alert-success"));
+			
 		}
 		
 		else {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body( "Email sent failed");
+			session.setAttribute("message", new Message("Email sent failed !! ", "alert-danger"));
 		}
-		
+		return "redirect:/user/sendmail";
 		
 	}
 }
